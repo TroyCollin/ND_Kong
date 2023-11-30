ba926d4b3b72350fd31b9116646c5777d002ccc2
Troy Collin <75144078+TroyCollin@users.noreply.github.com>
2023-11-30 16:15:07 -0500

    Update kong_new_client.sh
    Added echo out of the API key

--- kong_new_client.sh
+++ kong_new_client.sh
@@ -21,9 +21,27 @@ read -p "Enter the code for this consumer: " consumer_id
 
 
 user=$(curl -i -X POST --url http://localhost:8001/consumers/ --data "username=$consumer_name" --data "custom_id=$consumer_id" --data "tags=Prod")
-consumer_data=$(echo "$user" | grep id | cut -d'"' -f 12)
+echo "$user"
+consumer_data=$(echo "$user" | grep id | cut -d'"' -f 14)
+echo "$consumer_data"
 curl -X POST "http://127.0.0.1:8001/consumers/$consumer_data/acls" --data "group=Prod-AccessGroup"
 curl -X POST "http://127.0.0.1:8001/consumers/$consumer_data/acls" --data "group=UATaccess"
 curl -X POST "http://127.0.0.1:8001/consumers/$consumer_data" --data "group=Prod-Access-Positions"
 curl -X POST --url "http://localhost:8001/consumers/$consumer_data/key-auth/"
+
+
+# display API key
+key=$(curl -X GET --url "http://localhost:8001/consumers/$consumer_data/key-auth/")
+key1=$(echo "$key" | cut -d'"' -f 12)
+
+echo ""
+echo "Here is the API Key"
+echo ""
+echo " ******************************************"
+echo " *                                        * "
+echo "   $key1"
+echo " *                                        *"
+echo " ******************************************"
+echo ""
+
 
